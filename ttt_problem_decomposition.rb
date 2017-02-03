184c4771abfff425b527d35be88c1fdf83e135f1
Riaz Mohamed <railsdevriaz@gmail.com>
2017-02-03 05:48:20 +0000

    Set up the initial board for tictactoe

---
+++ ttt_problem_decomposition.rb
@@ -0,0 +1,19 @@
+# ttt_problem_decomposition.rb
+
+=begin
+  Tic Tac Toe is a 2 player game played on a 3x3 board. Each player takes a turn and marks a square on the board. First player to reach a 3 squares in a row, including diagonals, wins. If all 9 squares are marked and no player has 3 squares in a row, then the game is a tie. 
+=end
+
+=begin
+  outlining the sequence of the gameplay
+  1. Display the initial empty 3x3 board.
+  2. Ask the user to mark a square.
+  3. Computer marks a square.
+  4. Display the updated board state.
+  5. If winner, display the winner.
+  6. if the board is full, display tie.
+  7. If neither winner nor board is full, go to #2
+  8. Play again?
+  9. If yes, go to #1
+  10. Good Bye!
+=end
