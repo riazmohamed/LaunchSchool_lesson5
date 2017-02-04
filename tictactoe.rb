460712dfe2864a21c4b4761029e959fcf44ec8f1
Riaz Mohamed <railsdevriaz@gmail.com>
2017-02-04 06:48:26 +0000

    Add rubocop implementation

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,6 +2,10 @@
 
 require 'pry'
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -10,8 +14,10 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize.
 def display_board(brd)
   system "clear"
+  puts "You're a #{PLAYER_MARKER}, computer is a #{COMPUTER_MARKER}"
   puts " "
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -26,6 +32,7 @@ def display_board(brd)
   puts "     |     |"
   puts " "
 end
+# rubocop:enable Metrics/AbcSize.
 
 def initialize_board
   new_board = {}
@@ -39,7 +46,7 @@ end
 
 def player_places_piece!(brd)
   square = ''
-  loop  do
+  loop do
     prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
@@ -59,17 +66,41 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  false
+  !!detect_winner(brd) # here the !! will turn the output to a boolean
 end
 
-board = initialize_board
-display_board(board)
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    return 'Player' if brd.values_at(*line).count(PLAYER_MARKER) == 3
+    return 'Computer' if brd.values_at(*line).count(COMPUTER_MARKER) == 3
+  end
+  nil
+end
 
 loop do
-  player_places_piece!(board)
-  computer_places_piece!(board)
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
-  break if someone_won?(board) || board_full?(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board
+prompt "Thanks for playing Tic Tac Toe, Goodbye!"
